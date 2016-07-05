3905eaf3cd5eec887453e7c570d1677fad6701bf
Konno <dra.azuki@gmail.com>
2016-07-05 01:06:10 +0000

    [modify] コメント付与

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,15 +1,20 @@
 class ExhibitsController < ApplicationController
+  # ログイン済みのユーザのみアクセス可能とするアクション
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # @exhibit = Exhibit.find(params[:id])が必要なアクション
   before_action :set_exhibit, only: [:show, :edit, :update, :destroy]
 
+  # 商品一覧アクション
   def index
     @exhibits = Exhibit.all.order(created_at: :desc)
   end
 
+  # 商品新規登録newアクション
   def new
     @exhibit = Exhibit.new
   end
 
+  # 商品新規登録createアクション
   def create
     @exhibit = Exhibit.new(exhibit_params)
     if @exhibit.save
@@ -19,17 +24,21 @@ class ExhibitsController < ApplicationController
     end
   end
 
+  # 商品編集editアクション
   def edit
   end
 
+  # 商品編集updateアクション
   def update
     @exhibit.update(exhibit_params)
     redirect_to @exhibit
   end
 
+  # 商品詳細showアクション
   def show
   end
 
+  # 商品削除destroyアクション
   def destroy
     @exhibit.destroy
     redirect_to exhibits_url
@@ -37,10 +46,12 @@ class ExhibitsController < ApplicationController
 
   private
 
+  # exhibitストロングパラメータ
   def exhibit_params
     params.require(:exhibit).permit(:user_id, :image, :image_cache, :title, :exhibit_description, :exhibit_category_id,:price, :book_status_id, :exhibit_status_id)
   end
 
+  # 共通アクション set_exhibit
   def set_exhibit
     @exhibit = Exhibit.find(params[:id])
   end
