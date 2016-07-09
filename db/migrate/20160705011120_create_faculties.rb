24521be2ed41f1b64447825faa52b423da518f80
Ryota Konno <r.konno@nowall.co.jp>
2016-07-09 13:59:57 +0900

    [modify] Exhibit indexページの左サイドメニューのデザインを変更

--- db/migrate/20160705011120_create_faculties.rb
+++
@@ -1,9 +0,0 @@
-class CreateFaculties < ActiveRecord::Migration
-  def change
-    create_table :faculties do |t|
-      t.string :faculty_name
-
-      t.timestamps null: false
-    end
-  end
-end
