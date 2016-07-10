d759deba69f16140f35025c4804aef063ca816dd
Ryota Konno <r.konno@nowall.co.jp>
2016-07-10 13:11:36 +0900

    [add] deviceの新規会員登録、ログイン画面にデザインを適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:student_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:admission_year])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:faculty_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:department_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:student_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:admission_year])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:faculty_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:department_id])
+  end
 end
